818e9fcd4f9ba81d519e9091c2fdaa914331c1ec
magicat-toolkit <lucacavallaro@gmail.com>
2025-08-05 15:59:30 +0200

    setuped the object cells

---
+++ src/cells/CellInterface.java
@@ -0,0 +1,10 @@
+package cells;
+
+public interface CellInterface {
+
+    char simbol = '?';
+
+    void cellFunction();
+
+
+}
--- src/main/Main.java
+++ src/main/Main.java
@@ -47,16 +47,6 @@ public class Main {
                 System.out.println("ERROR: insert a things in the action bar!");
             }
 
-
-
-
-
-
-
-
-
-
-
         }
 
     }
